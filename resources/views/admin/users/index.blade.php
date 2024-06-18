1f528512166155c328677e13a75e91e35aa81191
Pedro Castro <pvmcastro@gmail.com>
2024-06-18 19:52:23 -0300

    changed View Users to list all users in database in a table

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -3,9 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=h1, initial-scale=1.0">
-    <title>Document</title>
+    <title>Usuarios</title>
 </head>
 <body>
-    <h1>Olá {{ $user->name }}!</h1>
+    <h1>Usuários</h1>
+
+    <!-- use esse comando: table>thread>tr>th*3 -->
+    <table>
+        <thread>
+            <tr>
+                <th>Nome</th>
+                <th>E-mail</th>
+                <th>Ações</th>
+            </tr>
+        </thread>
+        <tbody>
+            @forelse ($users as $user)
+                <tr>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>-</td>
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="100">Nenhum usuário no banco</td>
+                </tr>
+            @endforelse
+        </tbody>
+    </table>
 </body>
 </html>
