c9b3056cbbac84f6147baeedf8558e4878a6cee2
Pedro Castro <pvmcastro@gmail.com>
2024-06-18 20:25:27 -0300

    change the structure of the User view to add template dinamicamente

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,37 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=h1, initial-scale=1.0">
-    <title>Usuarios</title>
-</head>
-<body>
-    <h1>Usuários</h1>
+@extends('admin.layouts.template')
 
-    <!-- use esse comando: table>thread>tr>th*3 -->
-    <table>
-        <thread>
+@section('content')
+<h1>Usuários</h1>
+
+<!-- use esse comando: table>thread>tr>th*3 -->
+<table>
+    <thread>
+        <tr>
+            <th>Nome</th>
+            <th>E-mail</th>
+            <th>Ações</th>
+        </tr>
+    </thread>
+    <tbody>
+        @forelse ($users as $user)
             <tr>
-                <th>Nome</th>
-                <th>E-mail</th>
-                <th>Ações</th>
+                <td>{{ $user->name }}</td>
+                <td>{{ $user->email }}</td>
+                <td>-</td>
             </tr>
-        </thread>
-        <tbody>
-            @forelse ($users as $user)
-                <tr>
-                    <td>{{ $user->name }}</td>
-                    <td>{{ $user->email }}</td>
-                    <td>-</td>
-                </tr>
-            @empty
-                <tr>
-                    <td colspan="100">Nenhum usuário no banco</td>
-                </tr>
-            @endforelse
-        </tbody>
-    </table>
+        @empty
+            <tr>
+                <td colspan="100">Nenhum usuário no banco</td>
+            </tr>
+        @endforelse
+    </tbody>
+</table>
 
-    {{ $users->links() }} <!-- esse comando faz todo o css tailwind da paginação -->
-</body>
-</html>
+{{ $users->links() }} <!-- esse comando faz todo o css tailwind da paginação -->
+@endsection
