e67ba33aefdd5a508619a1b3f8ec408343a5f4e5
Pedro Castro <pvmcastro@gmail.com>
2024-06-19 10:58:12 -0300

    title correction

--- resources/views/admin/layouts/template.blade.php
+++ resources/views/admin/layouts/template.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title') - CRUD</title>
+    <title>@yield('title')</title>
 </head>
 <body>
     <header>header default</header>
