8a4f5cd2e851ab90434ddc2cc3ee836a051f84bf
Pedro Castro <pvmcastro@gmail.com>
2024-06-22 14:19:16 -0300

    voltando atrás e tirando os insert da migration create

--- database/migrations/2024_06_17_183006_create_products_table.php
+++ database/migrations/2024_06_17_183006_create_products_table.php
@@ -21,65 +21,6 @@ return new class extends Migration
             //$table->integer('stock')->default(0);
             $table->timestamps(); // datetime da criacao
         });
-
-        DB::table('products')->insert([
-            [
-                'name' => 'Café',
-                'description' => 'Café coado, 50ml',
-                'price' => 2.00,
-                'thumbnail' => 'cafe.jpg',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'name' => 'Pão de queijo',
-                'description' => '',
-                'price' => 4.00,
-                'thumbnail' => 'paoDeQueijo.jpg',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'name' => 'Salgado',
-                'description' => 'Coxinha de frango, Risole de carne, Esfiha de Frango, Esfiha de Carne, Esfiha de Presunto e Queijo, Pão de Batata c/ Requeijão',
-                'price' => 7.00,
-                'thumbnail' => 'salgado.jpg',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'name' => 'Sanduíche Natural',
-                'description' => 'Frango, maionese, cenoura, milho, passas e pão integral',
-                'price' => 10.00,
-                'thumbnail' => 'sanduicheNatural.jpg',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'name' => 'Misto Quente',
-                'description' => 'Queijo mussarela, presunto e pão de forma',
-                'price' => 10.00,
-                'thumbnail' => 'mistoQuente.jpg',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'name' => 'Mini Pizza',
-                'description' => 'Calabresa, Portuguesa, Quatro-Queijos e Frango com Catupiry',
-                'price' => 10.00,
-                'thubmnail' => 'miniPizza.jpg',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'name' => 'Coca-Cola',
-                'description' => 'Coquinha 200ml',
-                'price' => 3.50,
-                'thumbnail' => 'coquinha.jpg',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-        ]);
     }
 
     
