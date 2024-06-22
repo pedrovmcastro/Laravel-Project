9723798d6fafe501bbe2e4a73a80ac53438db874
Pedro Castro <pvmcastro@gmail.com>
2024-06-22 11:38:02 -0300

    $fillable da model Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,31 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'thumbnail',
+    ];
 }
+
+// O atributo $fillable em uma model do Laravel serve para definir os campos que podem ser “atribuídos em massa” (mass assignment). 
+// Isso significa que você pode passar vários valores de uma vez só para inserção no banco de dados. Por exemplo, suponha que você tenha um model User com os campos id, nome e idade.
+// Sem o $fillable, você precisaria criar um objeto e atribuir os valores um por um, como no exemplo abaixo:
+
+// $joao = new User();
+// $joao->nome = 'João';
+// $joao->idade = 20;
+// $joao->save();
+
+//No entanto, para facilitar esse cadastro, você pode definir o valor de $fillable com as colunas que deseja permitir. Por exemplo:
+
+// protected $fillable = ['nome', 'idade'];
+
+// Agora você pode criar um novo usuário assim:
+
+//$joao = User::create([
+//    'nome' => 'João',
+//    'idade' => 20
+// ]);
