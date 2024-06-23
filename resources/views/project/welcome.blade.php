64ada4343eb3bb6f2c52ecba195a1078453dc7d0
Pedro Castro <pvmcastro@gmail.com>
2024-06-23 01:18:26 -0300

    Att section titulo & botao "Novo Produto"

--- resources/views/project/welcome.blade.php
+++ resources/views/project/welcome.blade.php
@@ -2,8 +2,17 @@
 
 @section('content')
   <main>
+    
     <section class="py-5 text-center container">
-      <h1 class="fw-light">Sistema de Gerenciamento de Produtos</h1>
+      <div class="row py-lg-5">
+        <h1 class="fw-light">Sistema de Gerenciamento de Produtos</h1>
+        <div class="col-lg-6 col-md-8 mx-auto">
+          <p class="lead text-body-secondary">Transforme seu inventário em inteligência: gestão de produtos simplificada.</p>
+          <p>
+            <a href="{{Route("product.create")}}" class="btn btn-primary my-2">Novo Produto</a>
+          </p>
+        </div>
+      </div>
     </section>
 
     <div class="album py-5 bg-body-tertiary">
@@ -18,8 +27,10 @@
                   <p class="card-text">{{ $product->description }}</p>
                   <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
+                      @guest
                       <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
                       <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                      @endguest
                     </div>
                     <small class="text-muted">R$ {{ number_format($product->price, 2, ',', '.') }}</small>
                   </div>
